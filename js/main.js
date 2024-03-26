1bfd7b481b7ebfb16f48df0472456a37235e5832
Bektur <bekturnurlanov01@gmail.com>
2024-03-26 06:36:26 +0600

    added eventlistener to form, rewrited showMovies

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const API_KEY = "e83d87cc-f996-4dc8-8476-aaaab4bd2c71";
 const API_URL_PREMIERS =
   "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2024&month=JANUARY";
 
+const API_URL_SEARCH =
+  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+
 getMovies(API_URL_PREMIERS);
 
 async function getMovies(url) {
@@ -12,26 +15,62 @@ async function getMovies(url) {
     },
   });
   const respData = await resp.json();
+  console.log(respData);
   showMovies(respData);
 }
 
 function showMovies(data) {
   const moviesEl = document.querySelector(".movie__cards");
-  data.items.forEach((movie) => {
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("movie__card");
-    movieEl.innerHTML = `
-        <img src="${movie.posterUrlPreview}"
-            alt="${movie.nameRu}"
-            class="movie__img
-        />
-        <div class="movie__card__info">
-          <div class="movie__title">${movie.nameRu}</div>
-          <div class="movie__category">${movie.genres.map(
-            (genre) => `${genre.genre}`
-          )}</div>
-          <div class="movie__average movie__average-green">9</div>
-        </div>`;
-    moviesEl.appendChild(movieEl);
-  });
+  document.querySelector(".movie__cards").innerHTML = "";
+
+  if (data.items) {
+    data.items.forEach((movie) => {
+      const movieEl = document.createElement("div");
+      movieEl.classList.add("movie__card");
+      movieEl.innerHTML = `
+            <img src="${movie.posterUrlPreview}"
+                alt="${movie.nameEn || movie.nameRu}"
+                class="movie__img
+            />
+            <div class="movie__card__info">
+              <div class="movie__title">${movie.nameEn || movie.nameRu}</div>
+              <div class="movie__category">${movie.genres.map(
+                (genre) => ` ${genre.genre}`
+              )}</div>
+            </div>`;
+      moviesEl.appendChild(movieEl);
+    });
+  } else {
+    data.films.forEach((movie) => {
+      const movieEl = document.createElement("div");
+      movieEl.classList.add("movie__card");
+      movieEl.innerHTML = `
+          <img src="${movie.posterUrlPreview}"
+              alt="${movie.nameEn || movie.nameRu}"
+              class="movie__img
+          />
+          <div class="movie__card__info">
+            <div class="movie__title">${movie.nameEn || movie.nameRu}</div>
+            <div class="movie__category">${movie.genres.map(
+              (genre) => ` ${genre.genre}`
+            )}</div>
+          </div>`;
+      moviesEl.appendChild(movieEl);
+    });
+  }
 }
+
+const form = document.querySelector("form");
+const search = document.querySelector(".header__search");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
+  console.log(apiSearchUrl);
+  if (search.value) {
+    getMovies(apiSearchUrl);
+    search.value = "";
+  } else {
+    alert("Please enter a movie name");
+  }
+});
