4bf3fdf31c550bc049c942c279b622807987847d
Bektur <bekturnurlanov01@gmail.com>
2024-03-26 08:03:42 +0600

    adde eventlisteners to other chapters

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 const API_KEY = "e83d87cc-f996-4dc8-8476-aaaab4bd2c71";
 const API_URL_PREMIERS =
   "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2024&month=JANUARY";
-
 const API_URL_SEARCH =
   "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
-
 const API_URL_RELEASES =
-  "https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=2000&month=JANUARY";
+  "https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=2024&month=JANUARY";
+const API_URL_TOP_BESTS =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES";
+const TOP_EXPECTED =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FILMS";
 
-getMovies(API_URL_PREMIERS);
+  getMovies(API_URL_PREMIERS);
 
 async function getMovies(url) {
   try {
@@ -49,14 +51,20 @@ function showMovies(data) {
                 alt="${movie.nameEn || movie.nameRu}"
                 class="movie__img
             />
-            
               <div class="movie__title">${movie.nameEn || movie.nameRu}</div>
               <div class="movie__category">${movie.genres.map(
                 (genre) => ` ${genre.genre}`
               )}</div>
-              <div class="movie__average movie__average-${getClassByRate(
-                movie.rating
-              )}">${movie.rating}</div>
+              ${
+                `<div
+                  class="movie__average movie__average-${getClassByRate(
+                    movie.rating
+                  )}"
+                >
+                  ${movie.rating}
+                </div>` || ""
+              }
+              <img src="./images/favorite_heart.svg" alt="favorite" class="material-symbols-outlined"/>
             
             `;
       moviesEl.appendChild(movieEl);
@@ -65,9 +73,12 @@ function showMovies(data) {
 
   if (data.items) {
     displayHTML(data.items);
-  } else {
+  } else if{
     displayHTML(data.films);
-  }
+  } else if{
+    displayHTML(data.releases);
+  } else {alert("No movies found")
+    }
 }
 
 const form = document.querySelector("form");
@@ -86,7 +97,23 @@ form.addEventListener("submit", (e) => {
 });
 
 const releases = document.getElementById("releases");
+releases.addEventListener("click", () => {
+  getMovies(API_URL_RELEASES);
+});
+
 const premiers = document.getElementById("premiers");
+premiers.addEventListener("click", () => {
+  getMovies(API_URL_PREMIERS);
+});
+
 const top_expected = document.getElementById("top_expected");
+top_expected.addEventListener("click", () => {
+  getMovies(TOP_EXPECTED);
+});
+
 const top_best = document.getElementById("top_best");
+top_best.addEventListener("click", () => {
+  getMovies(API_URL_TOP_BESTS);
+});
+
 const favorites = document.getElementById("favorites");
